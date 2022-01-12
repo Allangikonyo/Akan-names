6ba43f200282b94c498c4bbd9423c4dc29b37ae5
Allan Gikonyo <gikonyo257@gmail.com>
2022-01-12 14:32:53 +0300

    akan form working

--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,55 @@
+
 document.getElementById("formAkan").addEventListener('submit', akan);
 
-// create a function that calculates the akan names
-function akan (){
-  // add names into an array 
-  var femaleNames = [Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama];
-  var maleNames = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame];
+
+function akan () {
+
+   // array declaration
+   let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+   let  maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+   // declaring variables
+    let day = parseInt(document.getElementById("Day").value );
+    let month = parseInt(document.getElementById("Month").value);  
+    let calendarYear = parseInt(document.getElementById("Year").value);
+    let gender = document.getElementById("Gender").value;
+
+    if(day<0 || day>31 || day == ''){
+      alert ("Valid day is required")
+      return;
+
+    }
+
+    else if (month<0 || month> 12 || month == ''){
+      alert ("Valid month is required")
+      return;
+
+    }
+
+    //splitting the full year
+    let Halfyear = calendarYear.toString().slice(-2);
+    let year = parseInt(Halfyear);
+    let century = Math.ceil((calendarYear)/100)-1;
+
+    // using calculation formula
+    let dayOfTheWeek= (((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day)%7;
+    let weekday = Math.floor(dayOfTheWeek);
+
+    if(gender =='Male'){
+         for (i=0; i <= 6; i++){
+         if (i==weekday){
+          alert("Your Akan Name is " + maleNames[i]);
+          
+       }
+      }
+    }
+    else{
+        for (i=0; i <= 6; i++){
+          if (i==weekday){
+           alert("Your Akan Name is " + femaleNames[i]);
+          
+          }
+        }
+      }
+  }
   
-// declare the variables as a constant 
-// return an element object whose id property matches the specified string
-// get the value from the input
-// change the value into an integer
- const calendarYear = parseInt(document.getElementById('Year').value);
- const month = parseInt(document.getElementById('Month').value);
- const day = parseInt(document.getElementById('Day').value );
- var gender = document.getElementById('Gender').value;
-
-
-// Splitting the year into the century
-  const year = parseInt(calendarYear.toString().slice(-2));
-  const century = Math.ceil ((calendarYear)/100)-1; 
-
-// using calculation formula 
-const dayOfTheWeek= (((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day)%7; 
-
-// gender 
- if (document.getElementById("gender").checked){
-   var gender = 'male';
-  
- }
-else {
-    var gender = 'female';
-} 
-
-// performs calculation on data and assigns akan name
-if (month <= 0 || month > 12 || month == ''){
-  //show error
-  message.push('Valid month is required');
-}
-else if ((day <= 0 || day > 32 || day == '')){
-  //show error
-  message.push(day, 'Valid day is required');
-} 
-else if (Math.round(dayOfTheWeek)==0 && gender === 'female') {
-  alert ("Your akan name is "+ femaleNames[0]);
-}
-
-else if (Math.round(dayOfTheWeek)==1 && gender === 'female') {
-  alert ("Your akan name is "+ femaleNames[1]);
-}
-
-else if (Math.round(dayOfTheWeek)==2 && gender === 'female') {
-  alert ("Your akan name is "+ femaleNames[2]);
-}
-
-else if (Math.round(dayOfTheWeek)==3 && gender === 'female') {
-  alert ("Your akan name is "+ femaleNames[3]);
-}
-
-else if (Math.round(dayOfTheWeek)==4 && gender === 'female') {
-  alert ("Your akan name is "+ femaleNames[4]);
-}
-
-else if (Math.round(dayOfTheWeek)==5 && gender === 'female') {
-  alert ("Your akan name is "+femaleNames[5]);
-}
-
-else if (Math.round(dayOfTheWeek)==6 && gender === 'female') {
-  alert ("Your akan name is "+femaleNames[6]);
-}
-
-else if (Math.round(dayOfTheWeek)==7 && gender === 'female') {
-  alert ("Your akan name is "+femaleNames[7]);
-}
-
-else if (Math.round(dayOfTheWeek)==0 && gender === 'male') {
-  alert ("Your akan name is "+ maleNames[0]);
-}
-
-else if (Math.round(dayOfTheWeek)==1 && gender === 'male') {
-  alert ("Your akan name is "+ maleNames[1]);
-}
-
-else if (Math.round(dayOfTheWeek)==2  && gender === 'male') {
-  alert ("Your akan name is "+maleNames[2]);
-}
-
-else if (Math.round(dayOfTheWeek)==3  && gender === 'male') {
-  alert ("Your akan name is "+maleNames[3]);
-}
-
-else if (Math.round(dayOfTheWeek)==4 && gender === 'male') {
-  alert ("Your akan name is "+maleNames[4]);
-}
-
-else if (Math.round(dayOfTheWeek)==5 && gender === 'male') {
-  alert ("Your akan name is "+maleNames[5]);
-}
-
-else if (Math.round(dayOfTheWeek)==6 && gender === 'male') {
-  alert ("Your akan name is "+maleNames[6]);
-}
-
-else if (Math.round(dayOfTheWeek)==7 && gender === 'male') {
-  alert ("Your akan name is "+maleNames[7]);
-}
-  
-else if (Math.round(dayOfTheWeek)==0 && gender === 'male') {
-  alert ("Your akan name is "+maleNames[0]);
-}
-
-} 
